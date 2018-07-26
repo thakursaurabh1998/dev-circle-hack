e4cd6a61a71efd795b747a2c291241b63c1b198b
Madhur Gupta <madhurgupta10@gmail.com>
2018-07-26 18:01:37 +0530

    Added facebook auto login

--- food-waste-management/src/Components/Register.js
+++ food-waste-management/src/Components/Register.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import Facebook from './facebook'
 
 class Register extends Component {
   render() {
-    return <div>Register</div>;
+    return ( <div>
+      <div
+        style = {
+          {
+            position: "fixed",
+            bottom: 100
+          }
+        } >
+        <Facebook />
+      </div>
+    </div>);
   }
 }
 
